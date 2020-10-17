b6ccbe2948164eacbb8d93f0c33e97d7fb22a60c
Isaque Maestri <60708221+imaestri@users.noreply.github.com>
2020-10-17 20:16:38 -0300

    Criando components,GlobalStyles e themes com Styled Components

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,18 @@
+export default {
+    title: 'dark', 
+
+
+    colors: {
+        primary: '#1b1f38',
+        secondary: '#252A48',
+        tertiary: '#313862',
+
+        white: '#fff',
+        black: '#000',
+        gray: '#BFBFBF',
+
+        success: '#4E41F0',
+        info: '#F79E1B',
+        warning: '#E44C4E'
+    }
+};
