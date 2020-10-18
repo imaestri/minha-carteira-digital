6c70a12906174b9077d6285b68e816e463a6f829
Isaque Maestri <60708221+imaestri@users.noreply.github.com>
2020-10-18 17:53:09 -0300

    Desenvolvendo o Aside e estilizando.

--- src/components/Aside/styles.tsx
+++ src/components/Aside/styles.tsx
@@ -4,7 +4,52 @@ export const Container = styled.div`
     grid-area: AS;
     color: ${props => props.theme.colors.white};
     background-color: ${props => props.theme.colors.secondary};
+    padding-left:20px;
+
+    border-right: 1px solid ${props => props.theme.colors.gray};
     
 `;
 
+export const Header = styled.header`
+    display: flex;
+    align-items:center;
+`;
 
+export const Title = styled.h3`
+    color: ${props => props.theme.colors.white};
+    margin-left: 10px;
+    margin-top: 18px;
+    font-size: 18px;
+`;
+export const LogImg = styled.img`
+    height: 32px;
+    width: 22px;
+    margin-top: 18px;
+`;
+export const MenuContainer = styled.nav`
+    display: flex;
+    flex-direction: column;
+    margin-top: 50px;
+    
+
+`;
+export const MenuItemLink = styled.a`
+    color: ${props => props.theme.colors.info};
+    text-decoration:none;
+    margin: 7px 0;
+    display:flex;
+    align-items:center;
+
+
+    transition: opacity .3s;
+
+    &:hover{
+        opacity: .7;
+    }
+
+    > svg{
+        font-size: 18px;
+        margin-right: 5px;
+    }
+
+`;
