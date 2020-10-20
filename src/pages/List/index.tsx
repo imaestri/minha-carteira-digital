fe48e8c03be8478d486e867818561a5888cce768
Isaque Maestri <60708221+imaestri@users.noreply.github.com>
2020-10-20 10:32:20 -0300

    Estilização da lista e do componente card.

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -3,90 +3,34 @@ import ContentHeader from '../../components/ContentHeader';
 import HistoryFinanceCard from '../../components/HistoryFinanceCard';
 import SelectInput from '../../components/SelectInput';
 
-import { Container, Content } from './styles';
+import { Container, Content, Filters } from './styles';
 
 const List: React.FC = () => {
-    const options = [
-        { value: 'Isaque', label: 'Isaque' },
-        { value: 'Leticia', label: 'Leticia' }
+    const months = [
+        { value: 7, label: 'Julho' },
+        { value: 8, label: 'Agosto' },
+        { value: 9, label: 'Setembro' }
     ]
 
-    const time = [
-        { value: 'Galo', label: 'Galo' },
-        { value: 'Vasco', label: 'Vasco' }
+    
+    const years = [
+        { value: 2020, label: 2020 },
+        { value: 2019, label: 2019 },
+        { value: 2018, label: 2018 }
     ]
+
     return (
         <Container>
             <ContentHeader title="Saídas" lineColor="#E44C4E">
-                <SelectInput options={options} />
-                <SelectInput options={time} />
+                <SelectInput options={months} />
+                <SelectInput  options={years}/>
             </ContentHeader>
+            <Filters>
+                <button type="button"className="tag-filter tag-filter-recurrent">Recorrentes</button>
+                <button type="button" className="tag-filter tag-filter-eventual">Eventuais</button>
+            </Filters>
             <Content>
-                <HistoryFinanceCard 
-                    cardColor="#313862"
-                    tagColor="#E44C4E"
-                    title="Conta de Luz"
-                    subtitle="01/01/2020"
-                    amount="R$ 113,00"
-                />
-                 <HistoryFinanceCard 
-                    cardColor="#313862"
-                    tagColor="#E44C4E"
-                    title="Conta de Luz"
-                    subtitle="01/01/2020"
-                    amount="R$ 113,00"
-                />
-                 <HistoryFinanceCard 
-                    cardColor="#313862"
-                    tagColor="#E44C4E"
-                    title="Conta de Luz"
-                    subtitle="01/01/2020"
-                    amount="R$ 113,00"
-                />
-                 <HistoryFinanceCard 
-                    cardColor="#313862"
-                    tagColor="#E44C4E"
-                    title="Conta de Luz"
-                    subtitle="01/01/2020"
-                    amount="R$ 113,00"
-                />
-                 <HistoryFinanceCard 
-                    cardColor="#313862"
-                    tagColor="#E44C4E"
-                    title="Conta de Luz"
-                    subtitle="01/01/2020"
-                    amount="R$ 113,00"
-                />
-                 <HistoryFinanceCard 
-                    cardColor="#313862"
-                    tagColor="#E44C4E"
-                    title="Conta de Luz"
-                    subtitle="01/01/2020"
-                    amount="R$ 113,00"
-                />
-                 <HistoryFinanceCard 
-                    cardColor="#313862"
-                    tagColor="#E44C4E"
-                    title="Conta de Luz"
-                    subtitle="01/01/2020"
-                    amount="R$ 113,00"
-                />
-                 <HistoryFinanceCard 
-                    cardColor="#313862"
-                    tagColor="#E44C4E"
-                    title="Conta de Luz"
-                    subtitle="01/01/2020"
-                    amount="R$ 113,00"
-                />
-                 <HistoryFinanceCard 
-                    cardColor="#313862"
-                    tagColor="#E44C4E"
-                    title="Conta de Luz"
-                    subtitle="01/01/2020"
-                    amount="R$ 113,00"
-                />
-                 <HistoryFinanceCard 
-                    cardColor="#313862"
+                <HistoryFinanceCard
                     tagColor="#E44C4E"
                     title="Conta de Luz"
                     subtitle="01/01/2020"
